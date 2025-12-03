7b88d279881c935d6cecb47b76ef64a33790f94f
Francisco Pereyra <francisco.pereyra.dmc3@gmail.com>
2025-12-03 19:08:21 -0300

    Add and edit a simple Hello World get request again x8

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const bcrypt = require('bcrypt');
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-// bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => 
-//     {
-//         console.log(hash + " <= this is the bcrypt hash");
-//     });
+
 
 
 //END_ASYNC
