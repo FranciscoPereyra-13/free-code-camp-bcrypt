b058ba6d9d861ee47c8a5e9e5406ebe86c997e9d
Francisco Pereyra <francisco.pereyra.dmc3@gmail.com>
2025-12-03 23:29:32 -0300

    Move bcrypt require to top for FCC

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+
+const bcrypt = require('bcrypt');
+
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -10,7 +13,6 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-var bcrypt = require('bcrypt');
 
 bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
   console.log(hash);
