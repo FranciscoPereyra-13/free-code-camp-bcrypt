a958ee24fe6784b1dcccfc2814ee53bc72b2635f
Francisco Pereyra <francisco.pereyra.dmc3@gmail.com>
2025-12-04 10:03:36 -0300

    Add hash and hash compare functions

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-fccTesting(app);
 const bcrypt      = require("bcrypt"); 
+fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
@@ -12,8 +12,19 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => 
+    {
+        console.log(hash);
 
+         bcrypt.compare(myPlaintextPassword, hash, (err, res) => 
+        {
+            console.log(res);
+        });
 
+        return hash;
+    });
+
+   
 //END_ASYNC
 
 //START_SYNC
