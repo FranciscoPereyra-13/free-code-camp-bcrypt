56081b2b63c5c8869f534009b7f2c3415a6801ee
Francisco Pereyra <francisco.pereyra.dmc3@gmail.com>
2025-12-03 23:00:04 -0300

    Fix: move bcrypt into FCC blocks only

--- server.js
+++ server.js
@@ -3,23 +3,26 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-var bcrypt = require('bcrypt');
-
-
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+var bcrypt = require('bcrypt');
+
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+  console.log(hash);
 
 
 //END_ASYNC
 
 //START_SYNC
+var bcrypt = require('bcrypt');
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
 
 //END_SYNC
